57c680f0bfdb6049f4ba2e6c5945ebb1587e9a55
DrigsterI <gabriel.joe@tptlive.ee>
2021-07-25 00:50:21 +0300

    Small fixes

--- src/main/java/me/drigster/foxyartifacts/events/foxEvent.java
+++ src/main/java/me/drigster/foxyartifacts/events/foxEvent.java
@@ -24,7 +24,7 @@ public class foxEvent implements Listener {
     @EventHandler
     public void onEntityDeath(EntityDeathEvent e) {
         LivingEntity dead = e.getEntity();
-        Entity killer = dead.getKiller();
+        Player killer = dead.getKiller();
         if (dead.getType().equals(EntityType.FOX)) {
             if (killer == null) {
                 return;
@@ -32,36 +32,34 @@ public class foxEvent implements Listener {
             Location location = killer.getLocation();
             WorldServer world = ((CraftWorld) killer.getWorld()).getHandle();
             List<foxGod> foxes = new ArrayList<>();
-            if (killer instanceof Player) {
-                if (((Player) killer).getGameMode().equals(GameMode.CREATIVE)) {
-                    return;
-                }
-                Bukkit.broadcastMessage(killer.getName() + " вызвал небесную кару!");
-                int r = 4;
-                Location l = location.subtract(r, 0, r / 2);
-                for (double i = 0; i < 2 * Math.PI; i = i + (Math.PI / 3)) {
-                    double x = Math.sin(i) * r;
-                    double z = Math.cos(i) * r;
+            if (killer.getGameMode().equals(GameMode.CREATIVE)) {
+                return;
+            }
+            Bukkit.broadcastMessage(killer.getName() + " вызвал небесную кару!");
+            int r = 4;
+            Location l = location.subtract(r, 0, r / 2.0);
+            for (double i = 0; i < 2 * Math.PI; i = i + (Math.PI / 3)) {
+                double x = Math.sin(i) * r;
+                double z = Math.cos(i) * r;
 
-                    DecimalFormat df = new DecimalFormat("#.00");
+                DecimalFormat df = new DecimalFormat("#.00");
 
-                    l = l.add(x, 0, z);
-                    foxGod fox = new foxGod(l);
-                    fox.setGoalTarget((EntityLiving) ((CraftPlayer) killer).getHandle());
-                    world.addEntity(fox);
-                    foxes.add(fox);
-                }
-                BukkitScheduler scheduler = plugin.getServer().getScheduler();
-                for (foxGod fox : foxes) {
-                    scheduler.runTaskLater(plugin, new Runnable() {
-                        @Override
-                        public void run() {
-                            ((CraftPlayer) killer).playSound(fox.getBukkitEntity().getLocation(), Sound.ENTITY_ILLUSIONER_CAST_SPELL, 2, 1);
-                            ((CraftPlayer) killer).spawnParticle(Particle.EXPLOSION_NORMAL, fox.locX(), fox.locY(), fox.locZ(), 60);
-                            world.removeEntity(fox);
-                        }
-                    }, 20 * 10);
-                }
+                l = l.add(x, 0, z);
+                foxGod fox = new foxGod(l);
+                fox.setGoalTarget(((CraftPlayer) killer).getHandle());
+                world.addEntity(fox);
+                foxes.add(fox);
+            }
+            BukkitScheduler scheduler = plugin.getServer().getScheduler();
+            for (foxGod fox : foxes) {
+                scheduler.runTaskLater(plugin, new Runnable() {
+                    @Override
+                    public void run() {
+                        (killer.playSound(fox.getBukkitEntity().getLocation(), Sound.ENTITY_ILLUSIONER_CAST_SPELL, 2, 1);
+                        killer.spawnParticle(Particle.EXPLOSION_NORMAL, fox.locX(), fox.locY(), fox.locZ(), 60);
+                        world.removeEntity(fox);
+                    }
+                }, 20 * 10);
             }
         }
     }
