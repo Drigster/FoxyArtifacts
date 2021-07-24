2749be558ca2e3bd15ec793868944ffef2b6af9d
DrigsterI <gabriel.joe@tptlive.ee>
2021-07-25 00:52:57 +0300

    Small fixes

--- src/main/java/me/drigster/foxyartifacts/events/foxEvent.java
+++ src/main/java/me/drigster/foxyartifacts/events/foxEvent.java
@@ -2,7 +2,6 @@ package me.drigster.foxyartifacts.events;
 
 import me.drigster.foxyartifacts.FoxyArtifacts;
 import me.drigster.foxyartifacts.mobs.foxGod;
-import net.minecraft.server.v1_16_R3.EntityLiving;
 import net.minecraft.server.v1_16_R3.WorldServer;
 import org.bukkit.*;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
@@ -13,7 +12,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.scheduler.BukkitScheduler;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,8 +40,6 @@ public class foxEvent implements Listener {
                 double x = Math.sin(i) * r;
                 double z = Math.cos(i) * r;
 
-                DecimalFormat df = new DecimalFormat("#.00");
-
                 l = l.add(x, 0, z);
                 foxGod fox = new foxGod(l);
                 fox.setGoalTarget(((CraftPlayer) killer).getHandle());
@@ -52,13 +48,10 @@ public class foxEvent implements Listener {
             }
             BukkitScheduler scheduler = plugin.getServer().getScheduler();
             for (foxGod fox : foxes) {
-                scheduler.runTaskLater(plugin, new Runnable() {
-                    @Override
-                    public void run() {
-                        (killer.playSound(fox.getBukkitEntity().getLocation(), Sound.ENTITY_ILLUSIONER_CAST_SPELL, 2, 1);
-                        killer.spawnParticle(Particle.EXPLOSION_NORMAL, fox.locX(), fox.locY(), fox.locZ(), 60);
-                        world.removeEntity(fox);
-                    }
+                scheduler.runTaskLater(plugin, () -> {
+                    killer.playSound(fox.getBukkitEntity().getLocation(), Sound.ENTITY_ILLUSIONER_CAST_SPELL, 2, 1);
+                    killer.spawnParticle(Particle.EXPLOSION_NORMAL, fox.locX(), fox.locY(), fox.locZ(), 60);
+                    world.removeEntity(fox);
                 }, 20 * 10);
             }
         }
